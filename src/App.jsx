667456eb71dc47bf5c3bd2598faa29fc60e63ec5
TobyVB <98196156+TobyVB@users.noreply.github.com>
2023-04-06 14:04:18 +0800

    create products data and product component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavbarComponent from "./components/Navbar";
+import { Container } from "react-bootstrap";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cancel from "./pages/Cancel";
+import Store from "./pages/Store";
+import Success from "./pages/Success";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Container>
+      <NavbarComponent></NavbarComponent>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Store />} />
+          <Route path="success" element={<Success />} />
+          <Route path="cancel" element={<Cancel />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,11 @@
+import { Card, Button, Form, Row, Col } from "react-bootstrap";
+
+export default function ProductCard(props) {
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title></Card.Title>
+      </Card.Body>
+    </Card>
+  );
+}
