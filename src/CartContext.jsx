76f5625504b3a8c76a238462d93f3d3e7ae6b98a
TobyVB <98196156+TobyVB@users.noreply.github.com>
2023-04-07 17:04:35 +0800

    create functions for manipulating quanities in cart

--- src/CartContext.jsx
+++ src/CartContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react";
-import { productsArray } from "./productsStore";
+import { productsArray, getProductData } from "./productsStore";
 
 export const CartContext = createContext({
   items: [],
@@ -11,7 +11,7 @@ export const CartContext = createContext({
 });
 
 export function CartProvider({ children }) {
-  const [cartProducts, setCaartProducts] = useState([]);
+  const [cartProducts, setCartProducts] = useState([]);
 
   function getProductQuantity(id) {
     cartProducts.find((product) => product.id === id)?.quantity;
@@ -23,6 +23,49 @@ export function CartProvider({ children }) {
 
   function addOneToCart(id) {
     const quantity = getProductQuantity(id);
+
+    if (quantity === 0) {
+      // product is not in cart
+      setCartProducts([...cartProducts, { id: id, quantity: 1 }]);
+    } else {
+      // product is in cart
+      setCartProducts(
+        cartProducts.map((product) =>
+          product.id === id
+            ? { ...product, quantity: product.quantity + 1 }
+            : product
+        )
+      );
+    }
+  }
+  function deleteFromCart(id) {
+    setCartProducts((cartProducts) =>
+      cartProducts.filter((currentProduct) => {
+        return currentProduct.id != id;
+      })
+    );
+  }
+  function removeOneFromCart(id) {
+    const quantity = getProductQuantity(id);
+
+    if (quantity == 1) {
+      deleteFromCart(id);
+    } else {
+      setCartProducts(
+        cartProducts.map((product) =>
+          product.id === id
+            ? { ...product, quantity: product.quantity - 1 }
+            : product
+        )
+      );
+    }
+  }
+
+  function getTotalCost() {
+    let totalCost = 0;
+    cartProducts.map((cartItem) => {
+      const productData = getProductData(cartItem.id); //45:42 min on video
+    });
   }
 
   const contextValue = {
